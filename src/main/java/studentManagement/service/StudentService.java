301db52d62ac4e90e602bb4e74cd88629f3e86da
shreyansh <shrey20021302@gmail.com>
2022-01-15 16:15:08 +0530

    Spring MVC-lab solution

---
+++ src/main/java/studentManagement/service/StudentService.java
@@ -0,0 +1,16 @@
+package studentManagement.service;
+
+import java.util.List;
+
+import studentManagement.entity.Student;
+
+public interface StudentService {
+
+	public List<Student> findAll();
+
+	public Student findById(int theId);
+
+	public void save(Student thestudent);
+
+	public void deleteById(int theId);
+}
